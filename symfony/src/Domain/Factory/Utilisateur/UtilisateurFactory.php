40c4e0f8fe6a6ea8115edd38c73cbf7428669a8d
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-02-22 00:11:19 +0100

    Utilisateur : création de UtilisateurFactory et ajout de la méthode creer().

---
+++ symfony/src/Domain/Factory/Utilisateur/UtilisateurFactory.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Domain\Factory\Utilisateur;
+
+use App\Domain\Entity\Utilisateur\Utilisateur;
+
+class UtilisateurFactory
+{
+    public function creer(string $email, string $nom, string $prenom, string $abreviation): Utilisateur
+    {
+        return (new Utilisateur())
+            ->setEmail($email)
+            ->setNom($nom)
+            ->setPrenom($prenom)
+            ->setAbreviation($abreviation)
+            ->setPassword('mot_de_passe_a_reinitialiser')
+            ->setRoles(['ROLE_USER]']);
+    }
+}
