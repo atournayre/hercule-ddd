e3b89e1b45f19e6512cd5d9b2b1dcb52b2500626
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-02-21 20:21:24 +0100

    Utilisateur : la méthode liste() définie par UtilisateurRepositoryInterface peut lancer une UtilisateurNonTrouveException.

--- symfony/src/Domain/Entity/Utilisateur/Repository/UtilisateurRepositoryInterface.php
+++ symfony/src/Domain/Entity/Utilisateur/Repository/UtilisateurRepositoryInterface.php
@@ -2,10 +2,14 @@
 
 namespace App\Domain\Entity\Utilisateur\Repository;
 
+use App\Domain\DTO\Utilisateur\UtilisateurListeDTO;
+use App\Domain\Entity\Utilisateur\Exception\UtilisateurNonTrouveException;
+
 interface UtilisateurRepositoryInterface
 {
     /**
      * @return array|UtilisateurListeDTO[]
+     * @throws UtilisateurNonTrouveException
      */
     public function liste(): array;
 }
