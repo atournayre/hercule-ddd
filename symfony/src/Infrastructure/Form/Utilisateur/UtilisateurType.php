f33a83345b869f760e3b0121f9bb04683a55625f
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-02-22 20:33:23 +0100

    Utilisateur : ajout de contraintes dans le formulaire pour l'email (non vide et valide) et pour l'abréviation (3 caractères).

--- symfony/src/Infrastructure/Form/Utilisateur/UtilisateurType.php
+++ symfony/src/Infrastructure/Form/Utilisateur/UtilisateurType.php
@@ -4,21 +4,51 @@ namespace App\Infrastructure\Form\Utilisateur;
 
 use App\Application\VO\Utilisateur\UtilisateurFormVO;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Validator\Constraints\Email;
+use Symfony\Component\Validator\Constraints\Length;
+use Symfony\Component\Validator\Constraints\NotBlank;
 
 class UtilisateurType extends AbstractType
 {
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
+            ->add(
+                'email',
+                EmailType::class,
+                [
+                    'constraints' => [
+                        new NotBlank([
+                            'message' => 'Veuillez renseigner un email',
+                        ]),
+                        new Email([
+                            'message' => 'Veuillez renseigner un email valide.',
+                        ])
+                    ],
+                    'required' => false,
+                ]
+            )
             ->add('prenom')
             ->add('nom')
-            ->add('abreviation')
-            ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer'])
-            ;
+            ->add(
+                'abreviation',
+                TextType::class,
+                [
+                    'constraints' => [
+                        new Length([
+                            'min' => 3,
+                            'max' => 3,
+                            'exactMessage' => 'Veuillez renseigner une abréviation contenant {{ limit }} caractères.',
+                        ])
+                    ]
+                ]
+            )
+            ->add('enregistrer', SubmitType::class, ['label' => 'Enregistrer']);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
