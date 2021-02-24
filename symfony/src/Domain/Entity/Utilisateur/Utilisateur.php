b4430ca6c905a513da9d0f11d1180bbeaafc2a08
Aurélien Tournayre <aurelien.tournayre@gmail.com>
2021-02-24 12:06:07 +0100

    Utilisateur : création d'un utilisateur (entité, factory, tests).

---
+++ symfony/src/Domain/Entity/Utilisateur/Utilisateur.php
@@ -0,0 +1,103 @@
+<?php
+
+namespace App\Domain\Entity\Utilisateur;
+
+use Symfony\Component\Security\Core\User\UserInterface;
+
+class Utilisateur implements UserInterface
+{
+    const ROLE_PAR_DEFAUT = 'ROLE_USER';
+
+    protected $email;
+    protected $password;
+    protected $roles = [];
+    protected $nom;
+    protected $prenom;
+    protected $abreviation;
+
+    public function getRoles(): array
+    {
+        if (count($this->roles) === 0) {
+            $this->roles = [
+                self::ROLE_PAR_DEFAUT,
+            ];
+        }
+        return $this->roles;
+    }
+
+    public function setRoles(array $roles): self
+    {
+        $this->roles = $roles;
+        return $this;
+    }
+
+    public function getPassword(): string
+    {
+        return $this->password;
+    }
+
+    public function setPassword(string $password): self
+    {
+        $this->password = $password;
+        return $this;
+    }
+
+    public function getSalt()
+    {
+        return null;
+    }
+
+    public function getUsername()
+    {
+        return $this->email;
+    }
+
+    public function eraseCredentials()
+    {
+        // TODO: Implement eraseCredentials() method.
+    }
+
+    public function getEmail(): ?string
+    {
+        return $this->email;
+    }
+
+    public function setEmail(?string $email): self
+    {
+        $this->email = $email;
+        return $this;
+    }
+
+    public function getNom(): ?string
+    {
+        return $this->nom;
+    }
+
+    public function setNom(?string $nom): self
+    {
+        $this->nom = $nom;
+        return $this;
+    }
+
+    public function getPrenom(): ?string
+    {
+        return $this->prenom;
+    }
+
+    public function setPrenom(?string $prenom): self
+    {
+        $this->prenom = $prenom;
+        return $this;
+    }
+
+    public function getAbreviation(): ?string
+    {
+        return $this->abreviation;
+    }
+
+    public function setAbreviation(?string $abreviation): self
+    {
+        $this->abreviation = $abreviation;
+        return $this;
+    }
+}
---
+++ symfony/src/Domain/Factory/Utilisateur/UtilisateurFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Domain\Factory\Utilisateur;
+
+use App\Domain\Entity\Utilisateur\Utilisateur;
+
+class UtilisateurFactory
+{
+    const MOT_DE_PASSE_INITIAL = 'mot_de_passe_a_reinitialiser';
+
+    public function creerUnUtilisateur(
+        ?string $email = null,
+        ?string $nom = null,
+        ?string $prenom = null,
+        ?string $abreviation = null
+    ): Utilisateur
+    {
+        return (new Utilisateur())
+            ->setEmail($email)
+            ->setNom($nom)
+            ->setPrenom($prenom)
+            ->setAbreviation($abreviation)
+            ->setPassword(self::MOT_DE_PASSE_INITIAL)
+            ->setRoles(['ROLE_USER']);
+    }
+}
