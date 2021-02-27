f9ca91df24562837a55492a5778a73535749a0ce
atournayre <aurelien.tournayre@gmail.com>
2021-02-27 10:26:07 +0100

    Utilisateur : mise en place de la liste des utilisateurs dans l'interface.

--- symfony/src/Presentation/Controller/Utilisateur/UtilisateurController.php
+++ symfony/src/Presentation/Controller/Utilisateur/UtilisateurController.php
@@ -6,15 +6,29 @@ use App\Application\Service\Utilisateur\UtilisateurCreationService;
 use App\Application\Service\Utilisateur\UtilisateurListeService;
 use App\Application\Service\Utilisateur\UtilisateurModificationService;
 use App\Application\Service\Utilisateur\UtilisateurService;
+use App\Domain\Entity\Utilisateur\Exception\UtilisateurNonTrouveException;
+use Exception;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class UtilisateurController extends AbstractController
 {
+    const LISTE_EXCEPTION_MESSAGE = 'Une erreur s\'est produite lors de récupération de la liste des utilisateurs.';
+
     public function liste(UtilisateurListeService $utilisateurListe): Response
     {
-        exit('Not implemented');
+        try {
+            $listeUtilisateurs = $utilisateurListe->liste();
+        } catch (UtilisateurNonTrouveException $exception) {
+            $this->addFlash('error', $exception->getMessage());
+        } catch (Exception $exception) {
+            $this->addFlash('error', self::LISTE_EXCEPTION_MESSAGE);
+        }
+
+        return $this->render('utilisateur/liste.html.twig', [
+            'liste_utilisateurs' => $listeUtilisateurs ?? [],
+        ]);
     }
 
     public function creer(Request $request, UtilisateurCreationService $utilisateurCreationService): Response
