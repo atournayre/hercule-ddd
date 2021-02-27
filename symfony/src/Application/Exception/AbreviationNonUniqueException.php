a50b0a08ef9bb640550053eee0c95a147e65c17c
atournayre <aurelien.tournayre@gmail.com>
2021-02-27 12:02:34 +0100

    Utilisateur : AbreviationNonUniqueException prend maintenant en paramètre l'abréviation.

--- symfony/src/Application/Exception/AbreviationNonUniqueException.php
+++ symfony/src/Application/Exception/AbreviationNonUniqueException.php
@@ -3,8 +3,15 @@
 namespace App\Application\Exception;
 
 use Exception;
+use Throwable;
 
 class AbreviationNonUniqueException extends Exception
 {
+    const EXCEPTION_MESSAGE_PATTERN = 'L\'abréviation "%s" est déjà utilisée.';
 
+    public function __construct(?string $abreviation = null, $message = "", $code = 0, Throwable $previous = null)
+    {
+        parent::__construct($message, $code, $previous);
+        $this->message = sprintf(self::EXCEPTION_MESSAGE_PATTERN, $abreviation);
+    }
 }
